81d90da933f69176c06a676c4b0cb3c993d308df
tengbu-haoshou <tengbu.haoshou@gmail.com>
2024-05-24 16:12:40 +0800

    small improvement

--- src/main/java/cn/com/xxxx/fmockup/FmockupApplication.java
+++ src/main/java/cn/com/xxxx/fmockup/FmockupApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProdFmockupApplication {
+public class FmockupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProdFmockupApplication.class, args);
+		SpringApplication.run(FmockupApplication.class, args);
 	}
 
 }
---
+++ src/test/java/cn/com/xxxx/fmockup/FmockupApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xxxx.fmockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmockupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
